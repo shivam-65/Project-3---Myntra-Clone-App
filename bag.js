3d6a57aa271b68d268dd0fda52a55ecce4e70bc7
Shivam Padaliya <98576767+shivam-65@users.noreply.github.com>
2024-02-28 10:47:43 +0530

    update bag icon
    add functionality in bag icon like add and remove items

---
+++ bag.js
@@ -0,0 +1,64 @@
+
+
+let bagItemObjects;
+onLoad();
+
+function onLoad(){
+    loadBagItemObjects();
+    displayBagitems();
+}
+
+function loadBagItemObjects(){
+    bagItemObjects=bagItems.map(itemId => {
+        for(let i=0;i<items.length;i++){
+            if(itemId == items[i].id){
+                return items[i];
+            }
+        }
+    });
+}
+
+function displayBagitems(){
+    let containerElement=document.querySelector('.bag-items-container');
+    let innerHTML='';
+    bagItemObjects.forEach(bagItem => {
+        innerHTML+=generateItemHTML(bagItem);
+    });
+    containerElement.innerHTML=innerHTML;
+}
+
+function removeFromBag(itemId){
+    bagItems=bagItems.filter(bagItemId => bagItemId != itemId);
+    localStorage.setItem('bagItems', JSON.stringify(bagItems));
+    loadBagItemObjects();
+    displayBagIcon();
+    displayBagitems();
+}
+
+function generateItemHTML(item){
+    return `
+    <div class="bag-item-container">
+            <div class="item-left-part">
+              <img class="bag-item-img" src="../${item.image}">
+            </div>
+            <div class="item-right-part">
+              <div class="company">${item.company}</div>
+              <div class="item-name">${item.item_name}</div>
+              <div class="price-container">
+                <span class="current-price">Rs ${item.current_price}</span>
+                <span class="original-price">Rs ${item.original_price}</span>
+                <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
+              </div>
+              <div class="return-period">
+                <span class="return-period-days">${item.return_period} days</span> return available
+              </div>
+              <div class="delivery-details">
+                Delivery by
+                <span class="delivery-details-days">${item.delivery_date}</span>
+              </div>
+            </div>
+
+            <div class="remove-from-cart" onclick="removeFromBag(${item.id})">X</div>
+          </div>`
+}
+
