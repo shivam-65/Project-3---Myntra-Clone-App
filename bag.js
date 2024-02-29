a2973d739062394c77439acdea86ffb640164259
Shivam Padaliya <98576767+shivam-65@users.noreply.github.com>
2024-02-29 11:08:00 +0530

    generate bagsummary
    add functionality in bag summary

--- bag.js
+++ bag.js
@@ -1,42 +1,92 @@
-
-
+const CONVENIENCE_FEES=99;
 let bagItemObjects;
 onLoad();
 
-function onLoad(){
-    loadBagItemObjects();
-    displayBagitems();
+function onLoad() {
+  loadBagItemObjects();
+  displayBagitems();
+  displayBagSummary();
 }
 
-function loadBagItemObjects(){
-    bagItemObjects=bagItems.map(itemId => {
-        for(let i=0;i<items.length;i++){
-            if(itemId == items[i].id){
-                return items[i];
-            }
-        }
-    });
-}
+function displayBagSummary() {
+  let totalItem=bagItemObjects.length;
+  let totalMRP=0;
+  let totalDiscount=0;
+  let finalPayment=0;
 
-function displayBagitems(){
-    let containerElement=document.querySelector('.bag-items-container');
-    let innerHTML='';
+  if(totalItem==0){
+    totalMRP=0;
+    totalDiscount=0;
+    finalPayment=0;
+  }
+  else{
     bagItemObjects.forEach(bagItem => {
-        innerHTML+=generateItemHTML(bagItem);
-    });
-    containerElement.innerHTML=innerHTML;
+      totalMRP+=bagItem.original_price;
+      totalDiscount+=bagItem.original_price - bagItem.current_price;
+    })
+    finalPayment=totalMRP-totalDiscount+CONVENIENCE_FEES;
+  }
+
+  
+
+  let bagSummaryElement = document.querySelector(".bag-summary");
+  bagSummaryElement.innerHTML = `
+  <div class="bag-details-container">
+    <div class="price-header">PRICE DETAILS (${totalItem} Items) </div>
+    <div class="price-item">
+        <span class="price-item-tag">Total MRP</span>
+        <span class="price-item-value">₹${totalMRP}</span>
+    </div>
+    <div class="price-item">
+        <span class="price-item-tag">Discount on MRP</span>
+        <span class="price-item-value priceDetail-base-discount">-₹${totalDiscount}</span>
+    </div>
+    <div class="price-item">
+      <span class="price-item-tag">Convenience Fee</span>
+      <span class="price-item-value">₹99</span>
+    </div>
+    <hr>
+    <div class="price-footer">
+      <span class="price-item-tag">Total Amount</span>
+      <span class="price-item-value">₹ ${finalPayment}</span>
+    </div>
+  </div>
+  <button class="btn-place-order">
+    <div class="css-xjhrni">PLACE ORDER</div>
+  </button>
+  `;
 }
 
-function removeFromBag(itemId){
-    bagItems=bagItems.filter(bagItemId => bagItemId != itemId);
-    localStorage.setItem('bagItems', JSON.stringify(bagItems));
-    loadBagItemObjects();
-    displayBagIcon();
-    displayBagitems();
+function loadBagItemObjects() {
+  bagItemObjects = bagItems.map((itemId) => {
+    for (let i = 0; i < items.length; i++) {
+      if (itemId == items[i].id) {
+        return items[i];
+      }
+    }
+  });
 }
 
-function generateItemHTML(item){
-    return `
+function displayBagitems() {
+  let containerElement = document.querySelector(".bag-items-container");
+  let innerHTML = "";
+  bagItemObjects.forEach((bagItem) => {
+    innerHTML += generateItemHTML(bagItem);
+  });
+  containerElement.innerHTML = innerHTML;
+}
+
+function removeFromBag(itemId) {
+  bagItems = bagItems.filter((bagItemId) => bagItemId != itemId);
+  localStorage.setItem("bagItems", JSON.stringify(bagItems));
+  loadBagItemObjects();
+  displayBagIcon();
+  displayBagitems();
+  displayBagSummary();
+}
+
+function generateItemHTML(item) {
+  return `
     <div class="bag-item-container">
             <div class="item-left-part">
               <img class="bag-item-img" src="../${item.image}">
@@ -59,6 +109,5 @@ function generateItemHTML(item){
             </div>
 
             <div class="remove-from-cart" onclick="removeFromBag(${item.id})">X</div>
-          </div>`
+          </div>`;
 }
-
